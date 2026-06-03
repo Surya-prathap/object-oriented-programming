d6d6a841f8a34a6fe99ac0e57b9f70769087154d
Surya-prathap <13794prathap@gmail.com>
2026-06-03 10:29:24 +0530

    feat: Abstraction and Encapsulation

---
+++ src/com/java/oop/abstaction/InterfaceExample.java
@@ -0,0 +1,5 @@
+package com.java.oop.abstaction;
+
+public interface InterfaceExample {
+    String hello();
+}
---
+++ src/com/java/oop/abstaction/InterfaceExampleImpl.java
@@ -0,0 +1,7 @@
+package com.java.oop.abstaction;
+
+public class InterfaceExampleImpl implements InterfaceExample{
+    public String hello(){
+        return "hello";
+    }
+}
